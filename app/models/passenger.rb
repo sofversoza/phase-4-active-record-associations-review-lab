fa0f6943b7844919bbcb7039b2327e237621dd9c
sofiaversoza <sofversoza@gmail.com>
2022-02-03 21:46:58 -0700

    commit tree 24c1f2b974fbced4fee7b296f4a5a606d17addb5

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,14 @@
 class Passenger < ApplicationRecord
+    has_many :rides
+    has_many :taxis, through: :rides
+end
+
+class Ride < ApplicationRecord
+    belongs_to :taxi
+    belongs_to :passenger
+end
+
+class Taxi < ApplicationRecord
+    has_many :rides
+    has_many :passengers, through: :rides
 end
